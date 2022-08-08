e520c8c51e993a5a0d7496aa4e6deb2068ee3915
TheBigMort <thebigmort1249@gmail.com>
2022-08-07 20:20:58 -0500

    feat: footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,19 @@
+import Links from '@src/components/links';
 import type { FC } from 'react';
 
 const Footer: FC = () => {
-  return <div className="h-[100px] w-screen bg-black"></div>;
+  const external = Links.slice(2);
+  const internal = Links.slice(0, 2);
+  return (
+    <div className="flex flex-col items-center bg-black py-[1ch] text-center font-Strippy text-2xl text-white">
+      <div className="mb-[0.5ch] flex items-center space-x-[1ch]">
+        {external}
+      </div>
+      {internal}
+      <p className="py-2 font-SF-Pro text-[1ch] text-white opacity-[50%]">
+        © 2022 Maxwell Rux. All Rights Reserved.
+      </p>
+    </div>
+  );
 };
 export default Footer;
